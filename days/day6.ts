ae80244f99bd84dad933a03a1f314ac94d98ab91
Xavier Filippi <filippi.xavier@yahoo.fr>
2025-12-03 18:37:21 +0100

    Day 6, Star 2

--- days/day6.ts
+++ days/day6.ts
@@ -4,7 +4,7 @@ async function getInput(src: string): Promise<string[]> {
   return input.trim().split("\n").map((elem) => elem.trim());
 }
 
-function star1(input: string[]) {
+function letterCounter(input: string[]): Record<string, number>[] {
   const msgCounter: Record<string, number>[] = [];
   for (const line of input) {
     for (let i = 0, len = line.length; i < len; i++) {
@@ -18,18 +18,33 @@ function star1(input: string[]) {
       msgCounter[i][line[i]]++;
     }
   }
+  
+  return msgCounter;
+}
+
+function star1(msgCounter: Record<string, number>[]) {
   const msg = msgCounter.map((ch) => {
-      return Object.entries(ch).reduce((acc, current) =>
-        acc[1] < current[1] ? current : acc
-      )[0];
-    }).join("");
-  console.log(`The error corrected version of the message is: ${msg}`
-  );
+    return Object.entries(ch).reduce((acc, current) =>
+      acc[1] < current[1] ? current : acc
+    )[0];
+  }).join("");
+  console.log(`The error corrected version of the message is: ${msg}`);
+}
+
+function star2(msgCounter: Record<string, number>[]) {
+  const msg = msgCounter.map((ch) => {
+    return Object.entries(ch).reduce((acc, current) =>
+      acc[1] > current[1] ? current : acc
+    )[0];
+  }).join("");
+  console.log(`The error corrected version of the message using modified repetition is: ${msg}`);
 }
 
 export async function exec() {
   console.log("Day 6: Signals and Noise");
 
   const input = await getInput("./inputs/day6.txt");
-  star1(input);
+  const counter = letterCounter(input);
+  star1(counter);
+  star2(counter);
 }
